afe0afe42f01adbacdedd1c403aea60f87744a09
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-08-02 02:45:58 +0400

    index.js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ import App from "./app.js"; // Подключение app.js
 const app = ReactDOMClient.createRoot(document.getElementById("app")) //создаем путь где будем выводить контент и указываем его как константу
 
 app.render(<App />) //выводим в div <> то что внутри функции
+
+
+const tg = window.Telegram.WebApp //позволяет работать с тг апи
+
+tg.expand(); // растягивает на фулл скрин при запуске
+tg.disableVerticalSwipes(); // Отключает скролл
+
+// Получаем имя пользователя Telegram
+const username = tg.initDataUnsafe.user ? tg.initDataUnsafe.user.username : "Unknown User";
+
+// Заменяем текст "Babitovec" на имя пользователя
+document.addEventListener("DOMContentLoaded", function() {
+    const usernameElements = document.getElementsByClassName("username");
+    for (let i = 0; i < usernameElements.length; i++) {
+        usernameElements[i].textContent = username;
+    }
+});
