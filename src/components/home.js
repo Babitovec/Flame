2bffbf71b0af04f3c3e175c419e71da96bd0677e
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-08-03 23:14:49 +0400

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,5 @@
-import React from "react"
-import  "../css/home.css" 
-import Navigation from "../components/navigation.js";
+import React from "react";
+import "../css/home.css";
 
 // Images
 import background_filled_colour from '../img/background-filled-colour.png';
@@ -10,42 +9,40 @@ import burn_logo from '../img/Burn-logo.png';
 import tg_background from '../img/tg_background.png';
 import gift_emoji_animated from '../img/gift_emoji_animated.gif';
 
-
 class Home extends React.Component {
-    render() {
-      return (
-        <div>
-            <div className="container">
-                <div className="score-stats">
-                    <img src={background_filled_colour} alt="background_filled_colour" className="score-background" />
-                    <div className="score-stats-box">
-                    <img src={burn_emoji_animated} alt="burn" className="burn-emoji-score" />
-                    <a href="/score_story.html" className="score-stats-text">Your Score</a>
-                    </div>
-                </div>
-        
-                <div className="profile">
-                    <img src={PFP} alt="PFP" className="profile-pic" />
-                    <div className="username">?</div>
-                    <div className="score">
-                    <img src={burn_logo} alt="burn" className="burn-logo-score" />
-                    <span className="score-count">5738</span>
-                    </div>
-                </div>
-        
-                <div className="chests">
-                    <img src={tg_background} alt="chests" className="chests-background" />
-                    <div className="in-chests">
-                    <span className="gifts-header">Gifts</span>
-                    <img src={gift_emoji_animated} alt="gift" className="chest" />
-                    <span className="open-gift">Open</span>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="score-stats">
+            <img src={background_filled_colour} alt="background_filled_colour" className="score-background" />
+            <div className="score-stats-box">
+              <img src={burn_emoji_animated} alt="burn" className="burn-emoji-score" />
+              <a href="/score_story.html" className="score-stats-text">Your Score</a>
             </div>
-            <Navigation />
+          </div>
+
+          <div className="profile">
+            <img src={PFP} alt="PFP" className="profile-pic" />
+            <div className="username">{this.props.username}</div>
+            <div className="score">
+              <img src={burn_logo} alt="burn" className="burn-logo-score" />
+              <span className="score-count">5738</span>
+            </div>
+          </div>
+
+          <div className="chests">
+            <img src={tg_background} alt="chests" className="chests-background" />
+            <div className="in-chests">
+              <span className="gifts-header">Gifts</span>
+              <img src={gift_emoji_animated} alt="gift" className="chest" />
+              <span className="open-gift">Open</span>
+            </div>
+          </div>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
