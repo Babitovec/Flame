736c971a919859aa7ee2c572556e18ad56e175ef
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-08-03 23:15:20 +0400

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,36 +6,43 @@ import tasks_icon from '../img/tasks_icon.png';
 import friends_icon from '../img/friends_icon.png';
 import stats_icon from '../img/stats_icon.png';
 
+const tg = window.Telegram.WebApp;
+
 class Navigation extends React.Component {
+  handleNavigation(page) {
+    tg.HapticFeedback.impactOccurred('light');
+    this.props.changePage(page);
+  }
+
   render() {
-    const { currentPage, changePage } = this.props;
+    const { currentPage } = this.props;
 
     return (
       <nav className="navigation">
         <div
           className={`nav-item ${currentPage === 'home' ? 'active' : ''}`}
-          onClick={() => changePage('home')}
+          onClick={() => this.handleNavigation('home')}
         >
           <img src={home_icon} alt="home" className="home_icon" />
           Home
         </div>
         <div
           className={`nav-item ${currentPage === 'tasks' ? 'active' : ''}`}
-          onClick={() => changePage('tasks')}
+          onClick={() => this.handleNavigation('tasks')}
         >
           <img src={tasks_icon} alt="tasks" className="tasks_icon" />
           Tasks
         </div>
         <div
           className={`nav-item ${currentPage === 'frens' ? 'active' : ''}`}
-          onClick={() => changePage('frens')}
+          onClick={() => this.handleNavigation('frens')}
         >
           <img src={friends_icon} alt="friends" className="friends_icon" />
           Frens
         </div>
         <div
           className={`nav-item ${currentPage === 'stats' ? 'active' : ''}`}
-          onClick={() => changePage('stats')}
+          onClick={() => this.handleNavigation('stats')}
         >
           <img src={stats_icon} alt="stats_icon" className="stats_icon" />
           Stats
@@ -46,4 +53,3 @@ class Navigation extends React.Component {
 }
 
 export default Navigation;
-
