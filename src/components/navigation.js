a3a7a660eb7d306113d3defdc87487cbfd2b62b5
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-08-03 22:52:23 +0400

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../css/navigation.css";
 
-// Navigation icons
 import home_icon from '../img/home_icon.png';
 import tasks_icon from '../img/tasks_icon.png';
 import friends_icon from '../img/friends_icon.png';
@@ -9,21 +8,35 @@ import stats_icon from '../img/stats_icon.png';
 
 class Navigation extends React.Component {
   render() {
+    const { currentPage, changePage } = this.props;
+
     return (
       <nav className="navigation">
-        <div className="nav-item" onClick={() => this.props.changePage('home')}>
+        <div
+          className={`nav-item ${currentPage === 'home' ? 'active' : ''}`}
+          onClick={() => changePage('home')}
+        >
           <img src={home_icon} alt="home" className="home_icon" />
           Home
         </div>
-        <div className="nav-item" onClick={() => this.props.changePage('tasks')}>
+        <div
+          className={`nav-item ${currentPage === 'tasks' ? 'active' : ''}`}
+          onClick={() => changePage('tasks')}
+        >
           <img src={tasks_icon} alt="tasks" className="tasks_icon" />
           Tasks
         </div>
-        <div className="nav-item" onClick={() => this.props.changePage('frens')}>
+        <div
+          className={`nav-item ${currentPage === 'frens' ? 'active' : ''}`}
+          onClick={() => changePage('frens')}
+        >
           <img src={friends_icon} alt="friends" className="friends_icon" />
           Frens
         </div>
-        <div className="nav-item" onClick={() => this.props.changePage('stats')}>
+        <div
+          className={`nav-item ${currentPage === 'stats' ? 'active' : ''}`}
+          onClick={() => changePage('stats')}
+        >
           <img src={stats_icon} alt="stats_icon" className="stats_icon" />
           Stats
         </div>
@@ -33,3 +46,4 @@ class Navigation extends React.Component {
 }
 
 export default Navigation;
+
